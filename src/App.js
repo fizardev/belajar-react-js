149f36065b5b8550579c481fd5fa07c95ab49bf9
fizardev <fizardev@gmail.com>
2021-10-19 10:14:21 +0700

    function component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Header from "./Header";
 
+const Footer = () => {
+  return(
+    <div>
+      <h4>Ini Footer</h4>
+      <p>@2018</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <h1>Halo Semuanya</h1>
+      <Footer/>
     </div>
   );
 }

2b52c652cdf05fab98fa87e3787c29af579a562d
fizardev <fizardev@gmail.com>
2021-10-19 15:25:54 +0700

    tugas 2 React JS

--- src/Page/Header.js
+++ src/Page/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
     return(
         <div>
-            <a href="">Home</a>|<a href="">Product</a>|<a href="">Kontak</a>|<a href="">Tentang Kami</a>
+            <center>
+                <a href="">Home</a>|<a href="">Product</a>|<a href="">Kontak</a>|<a href="">Tentang Kami</a>
+            </center>
         </div>
     );
 }

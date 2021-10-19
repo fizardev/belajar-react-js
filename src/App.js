f07dbe73dd5ef616c91db1c181587564921b98b6
fizardev <fizardev@gmail.com>
2021-10-19 09:46:11 +0700

    first installation react js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <h1>Hello React</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste molestiae est, quam, rem incidunt obcaecati inventore provident suscipit, hic eius architecto natus. Iusto placeat praesentium unde deleniti quaerat. Fugiat, saepe!</p>
     </div>
   );
 }

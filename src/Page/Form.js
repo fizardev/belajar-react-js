298164dfd00cf35fb80843a85380dec6d61d0ae2
fizardev <fizardev@gmail.com>
2021-11-02 09:25:33 +0700

    refs lanjutan

--- src/Page/Form.js
+++ src/Page/Form.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.textInput = React.createRef();
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.textInput = React.createRef();
+  }
 
-    handleChange(e) {
-        this.setState({
-            value: e.target.value,
-        });
-    }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value,
+    });
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        alert('Pesan input: ' + this.state.value);
-        this.textInput.current.focus();
-    }
-    
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" onChange={this.handleChange} ref={this.textInput} />
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        );
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    alert("Pesan input: " + this.state.value);
+    this.textInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            ref={this.textInput}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;

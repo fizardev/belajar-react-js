8458c672308ff19bf3e933c57466ee47fb12a4d8
fizardev <fizardev@gmail.com>
2021-11-02 09:26:48 +0700

    css inline in react

--- src/Page/Header.js
+++ src/Page/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const Header = () => {
-    return(
-        <div>
-            <center>
-                <h1>Belajar React JS</h1>
-                <a href="">Home</a>|<a href="">Product</a>|<a href="">Kontak</a>|<a href="">Tentang Kami</a>
-            </center>
-        </div>
-    );
-}
+  return (
+    <div>
+      <center>
+        <h1 style={{ color: "#ffffff", backgroundColor: "#ff3300" }}>
+          Belajar React JS
+        </h1>
+        <a href="">Home</a>|<a href="">Product</a>|<a href="">Kontak</a>|
+        <a href="">Tentang Kami</a>
+      </center>
+    </div>
+  );
+};
 
 export default Header;

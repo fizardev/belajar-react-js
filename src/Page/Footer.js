1144b00d35b2e9fd6d98ca1df58345db86255b1b
fizardev <fizardev@gmail.com>
2021-11-03 09:12:26 +0700

    css external on react js

--- src/Page/Footer.js
+++ src/Page/Footer.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import '../Style/style.css';
 const Footer = () => {
     return(
         <div>
-            <h4>Belajar React JS</h4>
+            <h4 id="footer">Belajar React JS</h4>
         </div>
     );
 }
